2188b13da614fd8e76df0d148a498145a12c483c
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-13 18:13:44 +0100

    EX3 with comments

--- EX3/EX3.rb
+++ EX3/EX3.rb
@@ -1,22 +1,25 @@
+#simple puts
 puts "I will not count my chickens"
-
+#puts with insetion in strings
 puts "Hens #{25 + 30 / 6}"
 puts "Roosters #{100 -25 *3 %4 }"
-
+#another statement
 puts "Now I will count the eggs"
-
+# simple maths calculation
 puts 3 + 2 + 1 -5 + 4 % 2 - 1 / 4 + 6
-
+#another question
 puts "Is it true that 3 + 2 < 5 - 7?"
-
+#a booleen puts
 puts 3 + 2 < 5 - 7
+#puts with embeded expression
 puts "What is 3 + 2? #{3 + 2}"
 puts "What is 5 - 7? #{5 - 7}"
-
+#answer
 puts "Oh, that's why it's false."
 
 puts "How about some more"
-
+#more questions with expressions
 puts "is it grather? #{5 > -2}"
 puts "Is it greater or equal? #{5 >= -2}"
 puts "Is it less or equal #{5 <= -2}"
+#end
