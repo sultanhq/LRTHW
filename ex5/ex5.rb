293b62d0c681a66e4a5a90a7b71dbc1eb0d221e0
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-16 00:21:31 +0100

    Excercise 5 with unit conversions

--- ex5/ex5.rb
+++ ex5/ex5.rb
@@ -1,17 +1,19 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie in 2009
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+height_cms = (height*2.54).to_i # conversion from inches to cm's
+weight = 180 # lbs
+weight_kgs = (weight*0.45359237).to_i # conversion fro US pounds to KG's
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-puts "Let's talk about #{my_name}."
-puts "He's #{my_height} inches tall."
-puts "He's #{my_weight} pounds heavy."
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall, which is #{height_cms} cm's tall"
+puts "He's #{weight} pounds heavy, which is #{weight_kgs} Kg's"
 puts "Actually that's not too heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair} hair."
-puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
 
 # this line is tricky, try to get it exactly right
-puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
