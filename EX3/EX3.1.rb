c426d2bec6c558b8ec7ef8522b6abdc48d1b0868
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-15 21:06:18 +0100

    EX3.rb rewritten with floats and EX3.1.rb example

--- EX3/EX3.1.rb
+++ EX3/EX3.1.rb
@@ -3,3 +3,8 @@ sum = 20.0 * 2.7 / 4.1
 puts sum
 #sum with floats rounded to integer
 puts sum.to_i
+
+puts 100 % 8
+#odd / even test
+puts 50 % 2
+puts 51 % 2
--- EX3/EX3.rb
+++ EX3/EX3.rb
@@ -2,15 +2,15 @@
 puts "I will not count my chickens"
 #puts with insetion in strings
 puts "Hens #{25 + 30 / 6}"
-puts "Roosters #{100 -25 *3 %4 }"
+puts "Roosters #{100.0 -25.0 *3.0 %4.0 }"
 #another statement
 puts "Now I will count the eggs"
 # simple maths calculation
-puts 3 + 2 + 1 -5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 -5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 #another question
 puts "Is it true that 3 + 2 < 5 - 7?"
 #a booleen puts
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2.0 < 5.0 - 7.0
 #puts with embeded expression
 puts "What is 3 + 2? #{3 + 2}"
 puts "What is 5 - 7? #{5 - 7}"
