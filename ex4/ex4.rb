5b8f13137e6ea85d2286051660cfabd880ce5154
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-15 21:44:22 +0100

    ex4 part 2 - comments added

--- ex4/ex4.rb
+++ ex4/ex4.rb
@@ -1,5 +1,5 @@
-cars = 100
-space_in_a_car = 4.0
+cars = 76
+space_in_a_car = 4
 drivers = 30
 passengers = 90
 cars_not_driven = cars - drivers
@@ -13,3 +13,5 @@ puts "There will be #{cars_not_driven} empty cars today."
 puts "We can transport #{carpool_capacity} people today"
 puts "We have #{passengers} to carpool today."
 puts "We need to put about #{average_passengers_per_car} in each car."
+
+# line 2 states using a float, this is not truely nessecary as there can only be a whole person, not part of one.
