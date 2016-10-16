bc92b923c5fd2f41e8a789eaa20955784bc91ac3
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-16 14:58:41 +0100

    found ex4 & 5 didn't stage in previous commit

--- ex4/ex4.rb
+++ ex4/ex4.rb
@@ -1,10 +1,18 @@
-cars = 76
+#variable for number of cars
+cars = 100
+#variable for number spaces in a car
 space_in_a_car = 4
+#variable for number of available drivers
 drivers = 30
+#variable for number of passengers
 passengers = 90
+#variable calculation for number of cars spare (without drivers)
 cars_not_driven = cars - drivers
+#variable for number of cars that can be driven
 cars_driven = drivers
+#variable for the total number passengers requiring transport
 carpool_capacity = cars_driven * space_in_a_car
+#variable calulating the number of passengers to put in each car.
 average_passengers_per_car = passengers / cars_driven
 
 puts "There are #{cars} cars available."
--- ex5/ex5.rb
+++ ex5/ex5.rb
@@ -1,19 +1,19 @@
-name = 'Zed A. Shaw'
+name = 'Rob Adams'
 age = 32 # not a lie in 2009
 height = 66.5 # inches
 height_cms = (height*2.54).to_i # conversion from inches to cm's
 weight = 137 # lbs
 weight_kgs = (weight*0.45359237).to_i # conversion fro US pounds to KG's
-eyes = 'Blue'
-teeth = 'White'
-hair = 'Brown'
+eyes = 'Grey'
+teeth = 'Nearly White'
+hair = 'Light Brown (almost ginger)'
 
-puts "Let's talk about #{name}."
-puts "He's #{height} inches tall, which is #{height_cms} cm's tall"
-puts "He's #{weight} pounds heavy, which is #{weight_kgs} Kg's"
+puts "Let's talk about me... #{name}."
+puts "I am #{height} inches tall, which is #{height_cms} cm's tall"
+puts "I am #{weight} pounds heavy, which is #{weight_kgs} Kg's"
 puts "Actually that's not too heavy."
-puts "He's got #{eyes} eyes and #{hair} hair."
-puts "His teeth are usually #{teeth} depending on the coffee."
+puts "I have got #{eyes.downcase} eyes and #{hair.downcase} hair."
+puts "My teeth are usually #{teeth.downcase} depending on the coffee."
 
 # this line is tricky, try to get it exactly right
 puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
