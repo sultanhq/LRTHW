41b2521282edf0506ee62d65f715e0ad138b6cb1
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-17 10:19:44 +0100

    with comments

--- ex6/ex6.rb
+++ ex6/ex6.rb
@@ -1,21 +1,36 @@
+# Variable defining types_of_people
 types_of_people = 10
+# creating a concatinated string from a string and a variable
 x = "There are #{types_of_people} types of people."
+# variable with string
 binary = "binary"
+# variable with string
 do_not = "don't"
+# a second concatinated string
 y = "Those who know #{binary} and those who #{do_not}."
 
+# print value of x with new line after
 puts x
+# print value of x with new line after
 puts y
 
+# print concatinated string
 puts "I said: #{x}."
+# print concatinated string
 puts "I also said: '#{y}'."
 
+variable with booleen value
 hilarious = false
+# variable made of concatinated string with string and booleen
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+print #variable
 puts joke_evaluation
 
+# varible definded as a string
 w = "This is the left side of..."
+# varible definded as a string
 e = "a string with a right side."
 
+# print two strings concatinated together
 puts w + e
