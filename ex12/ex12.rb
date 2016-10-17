fe0848a01e67a2177fc045edea6e5adc183fd07a
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-17 18:45:25 +0100

    ex12 - prompting for numbers with study drills

--- ex12/ex12.rb
+++ ex12/ex12.rb
@@ -1,12 +1,18 @@
 print "Give me a number: "
-number = gets.chomp.to_i
+number = gets.chomp.to_f
 
 bigger = number * 100
 puts "A bigger Number is #{bigger}."
 
 print "Give me another number: "
 another = gets.chomp
-number = another.to_i
+number = another.to_f
 
 smaller = number / 100
 puts "A smaller number is #{smaller}."
+
+puts "How much money are you going to give me? "
+money = gets.chomp.to_f
+grattitude = (money / 100) * 10
+
+puts "Oh thanks for you generosity.\nHere's #{grattitude} which is 10% back as a show of grattitude"
