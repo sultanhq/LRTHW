11eea6ec361e180ede114a6fdb9e832e3ffd0101
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-17 16:54:14 +0100

    ex10 - escape characters with extra work

---
+++ ex10/ex10.rb
@@ -0,0 +1,30 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm a \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+puts "\n\n"
+mary_mary = "\u266B Mary \n mary quite contrarey\n\t How does your garden grow?"
+mary_mary_2 = "With silver bell's and cockle $'s"
+mary_mary_3 = """And pretty maids
+\t all dressed up in a row : \u266B
+\t\t *Maid
+\t\t *Maid
+\t\t *Maid
+\t\t *Maid
+\t\t *Maid
+"""
+
+puts mary_mary
+puts mary_mary_2
+puts mary_mary_3
