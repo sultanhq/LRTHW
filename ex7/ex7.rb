91c2218c9a81d6e62d8ce9661c995cb91a7a21cd
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-17 10:51:45 +0100

    ex7 with comments

--- ex7/ex7.rb
+++ ex7/ex7.rb
@@ -1,8 +1,13 @@
+# print string with a new line afterwards
 puts "Mary had a little lamb."
+# print string containing interpolation of a literal string with a new line afterwards
 puts "Its fleece was white as #{'snow'}"
+# print string with a new line afterwards
 puts "And everwhere that Mary went."
+# print string with a new line afterwards
 puts "." * 10 # what'd that do?
 
+# a lot of varaiables of single character strings
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,5 +22,7 @@ end11 = "e"
 end12 = "r"
 
 # watch that print vs. puts on this line what's it do?
+# "print" puts all of these veariables together and doesn't do a new line at the end
 print end1 + end2 + end3 + end4 + end5 + end6
+# "puts" then completes the rest and puts a new line at the end
 puts end7 + end8 + end9 + end10 + end11 + end12
