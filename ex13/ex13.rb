b712464bdad12b27d6fc0f103ceebe386909725b
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-17 21:28:30 +0100

    ex13 - unpacking variables

---
+++ ex13/ex13.rb
@@ -0,0 +1,6 @@
+first, second, third, fourth= ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+puts "The fourth varaible is the magic word...: #{fourth}"
