656e753a065ff06f20c5d5b65a81a802e99dfeab
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-17 21:32:50 +0100

    ex13 - unpacking variables with extra code

--- ex13/ex13.rb
+++ ex13/ex13.rb
@@ -4,3 +4,9 @@ puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
 puts "The fourth varaible is the magic word...: #{fourth}"
+
+puts "Enter First digit"
+first_digit = $stdin.gets.chomp.to_i
+puts "Enter second digit"
+second_digit = $stdin.gets.chomp.to_i
+puts "The sum of the first and second digit is #{(first_digit + second_digit)}."
