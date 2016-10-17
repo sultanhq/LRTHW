f6042ebd19cf83c2277ac2cada0094aa845c276d
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-17 10:26:29 +0100

    testing for how many strings in exercise with comment

--- ex6/ex6.rb
+++ ex6/ex6.rb
@@ -1,13 +1,13 @@
 # Variable defining types_of_people
 types_of_people = 10
 # creating a concatinated string from a string and a variable
-x = "There are #{types_of_people} types of people."
+x = "There are #{types_of_people.class} types of people."
 # variable with string
 binary = "binary"
 # variable with string
 do_not = "don't"
 # a second concatinated string
-y = "Those who know #{binary} and those who #{do_not}."
+y = "Those who know #{binary.class} and those who #{do_not.class}."
 
 # print value of x with new line after
 puts x
@@ -15,14 +15,14 @@ puts x
 puts y
 
 # print concatinated string
-puts "I said: #{x}."
+puts "I said: #{x.class}."
 # print concatinated string
-puts "I also said: '#{y}'."
+puts "I also said: '#{y.class}'."
 
-variable with booleen value
+# variable with booleen value
 hilarious = false
 # variable made of concatinated string with string and booleen
-joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+joke_evaluation = "Isn't that joke so funny?! #{hilarious.class}" # booleen is not a class, it is a comparator, so does not return correct answer.
 
 print #variable
 puts joke_evaluation
@@ -33,4 +33,4 @@ w = "This is the left side of..."
 e = "a string with a right side."
 
 # print two strings concatinated together
-puts w + e
+puts "#{w.class} + #{e.class}" # line required re-writing to compile to show the class type.
