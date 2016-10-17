7f1c4758e6339688266ae18a495e88923efa2170
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-17 13:09:09 +0100

    ex9 - strings with character escapes -typo corrected

--- ex9/ex9.rb
+++ ex9/ex9.rb
@@ -4,10 +4,10 @@ days = "Mon Rue Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 puts "Here are the days: #{days}"
-puts "here are the month: #{months}"
+puts "Here are the months: #{months}"
 
 puts """
-theres somehthing going on here.
+There's something going on here.
 With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
