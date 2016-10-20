b2b21a2d97a6b67dfa70f7414daa95118cee8520
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-20 21:51:19 +0100

    Exercise 33: While Loops - with study drills

--- ex33/ex33.rb
+++ ex33/ex33.rb
@@ -1,16 +1,29 @@
-i = 0
-numbers = []
+def loop(n,add_to)
+  i = 0
+  numbers = []
+  while i < n
+    puts "loop start"
+    puts "At the top i is #{i}"
+    numbers.push(i)
 
-while i < 6
-  puts "At the top i is #{i}"
-  numbers.push(i)
-
-  i += 1
-  puts "Numbers now: ", numbers
-  puts "At the bottom i is #{i}"
+    i += add_to
+    puts "Numbers now: ", numbers
+    puts "At the bottom i is #{i}"
+    puts "loop end"
+  end
 end
 
+def loop_2(n,add_to)
+  arr = []
+  (0..n).step(add_to){|i| arr << i }
+  arr
+end
+
+
+puts loop(10,3)
 puts "The numbers: "
+
+puts loop_2(10,2)
 
 # remember you can write this 2 other ways?
 numbers.each {|num| puts num }
