dbdba5893cc8758980a3d4447e9948a5eca6c5da
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-20 22:17:41 +0100

    Exercise 34: Accessing Elements of Arrays

---
+++ ex34/ex34.rb
@@ -0,0 +1,18 @@
+animals = ['bear', 'ruby', 'peacock', 'kangeroo', 'whale', 'platypus']
+#1 The animal at 1 is the second animal which is a ruby
+#2 The thirds animal is at 2 and is a peacock
+#3 The first animal is at 0 and is a bear
+#4 The animal at 3 is the fourth animal which is a kangeroo
+#5 The fifth animal is at 4 and is a whale
+#6 The animal at 2 is the third animal which is a peacock
+#7 The sixth animal is at 5 and is a platypus
+#8 The animal at 4 is the fifth animal which is a whale
+
+puts animals[1]
+puts animals[2]
+puts animals[0]
+puts animals[3]
+puts animals[4]
+puts animals[2]
+puts animals[5]
+puts animals[3]
