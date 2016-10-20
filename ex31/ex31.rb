d918b4ab5bd8c3cd606e13cf4a4523f63d065041
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-20 09:45:56 +0100

    Exercise 31: Making Decisions - Study drill part 1

--- ex31/ex31.rb
+++ ex31/ex31.rb
@@ -34,6 +34,25 @@ elsif door == "2"
     puts "The insanity rots your eyes into a pool of muck.  Good job!"
   end
 
+elsif door == "3"
+  puts "You have found a secret door..."
+  puts "Behind the door is a room full of chom choms, you see a sign which says..."
+  puts """\n\tPlease deposit no less than FIVE Namibirian Whozars\n\tand then you can take as many chom choms you want!!"""
+
+  whozars = $stdin.gets.chomp
+
+  if whozars >= "5"
+    puts "You deposit #{whozars} Whozars and take all the chom choms you heart desires"
+    puts "You live happily ever after."
+  elsif whozars > "0"
+    puts "You deposit #{whozars} Whozars tring to be cheap"
+    puts "A siren starts sounding and you run away in shame"
+  else
+    puts "You deposit  #{whozars}... this angers the gods..."
+    puts "A 10tonne wieght falls from the cieling and crushes you to death"
+    puts "The end"
+  end
+
 else
   puts "You stumble around and fall on a knife and die.  Good job!"
   end
