faf5231510702d506c071d8535576251064eb9d3
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-23 19:03:42 +0100

    Exercise 38: Doing Things to Arrays - With study Drills

--- ex38/ex38.rb
+++ ex38/ex38.rb
@@ -7,52 +7,36 @@ more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"
 
 # using math to make sure there's 10 items
 
-while stuff.length != 10 # length(stuff)
-  next_one = more_stuff.pop # pop(more_stuff)
+while stuff.length != 10 # Array.length(stuff) # Call "length" on the the stuff array
+  next_one = more_stuff.pop # Array.pop(more_stuff)
+  # Call "pop" on more_stuff array and assign it to the variable "next_one"
   puts "Adding: #{next_one}"
-  stuff.push(next_one) # push(stuff, next_one)
-  puts "There are #{stuff.length} items now."
+  stuff.push(next_one) # Array.push(stuff, next_one)
+  # Call "push" with "next_one" on the stuff array
+  puts "There are #{stuff.length} items now." # Array.length(stuff) # Call "length" on the the stuff array
 end
 
-puts "There we go: #{stuff}"
+puts "There we go: #{stuff}" # Call the stuff array and interpolate it into the string
 
 puts "Let's do some things with stuff."
 
-puts stuff[1]
-puts stuff[-1] # whoa! fancy
-puts stuff.pop()
-puts stuff.join(' ')
-puts stuff[3..5].join("#")
+puts stuff[1] # calling the 2nd item in the stuff array
+puts stuff[-1] # calling the last item of the stuff array
+puts stuff.pop() # calling pop on the stuff array
+puts stuff.join(' ') # calling 'join' on the stuff array with the argument of blank spaces
+puts stuff[3..5].join("#") # calling the 4th to the 6th item of the array and then calling join
+                           # on the return with the argument "#"
 
+robs_cars = ["Brown Astra", "Black Astra", "Yellow Astra", "Cavalier SRI", "Black Astra GTE", "Calibra 16v", "Omega v6", "Corsa CDTI", "Omega Diesel", "Insignia"]
 
-def push(array, new_element)
-  # put new_element in array
-end
-
-push([1,2,3], 4) #=> [1,2,3,4]
-
-class Array
-  def push(new_element)
-    self
-  end
-end
-
-[1,2,3].push(4)
-
-"aBcDeF"
+puts robs_cars.sort
 
 def mad_case(string)
   string.downcase.chars.map.with_index { |e, i| i.odd? ? e.upcase : e.downcase }.join
 end
 
-puts mad_case('abcdef')
-
-class String
-  def mad_case
-    self.downcase.chars.map.with_index { |e, i| i.odd? ? e.upcase : e.downcase }.join
-  end
+def madcase_array_items(array)
+  array.map{ |e| mad_case(e) }
 end
 
-puts 'DONFNGNGIREN'.downcase
-
-puts 'ghijklm'.mad_case
+puts madcase_array_items(robs_cars)
